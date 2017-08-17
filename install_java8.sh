4eaf4785b79b2e00dc32d9806ff257281a7b9bc8
Michael Whittaker <mjwhittaker@berkeley.edu>
2017-08-17 13:58:04 -0700

    Added IntelliJ installation.

--- install_java8.sh
+++ install_java8.sh
@@ -7,7 +7,7 @@ main() {
     readonly JAVA_B=11
     readonly JAVA_HASH="d54c1d3a095b4ff2b6607d096fa80163"
 
-    # JDK (http://stackoverflow.com/a/10959815/3187068).
+    # JDK (http://stackoverflow.com/a/10959815/3187068)
     wget \
         --progress=dot:giga \
         --no-check-certificate \
@@ -18,22 +18,31 @@ main() {
     echo 'export JAVA_HOME="$HOME/jdk1.8.0_'${JAVA_VERSION}'"' >> ~/.bashrc
     echo 'export PATH="$JAVA_HOME/bin:$PATH"'                  >> ~/.bashrc
 
-    # eclipse.
-    readonly ECLIPSE_TAR="eclipse-java-neon-3-linux-gtk-x86_64.tar.gz"
-    wget \
-        --progress=dot:giga \
-        "http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/neon/3/${ECLIPSE_TAR}"
-    tar -xzf "${ECLIPSE_TAR}"
-    echo 'export ECLIPSE_HOME="$HOME/eclipse"' >> ~/.bashrc
-    echo 'export PATH="$ECLIPSE_HOME:$PATH"'   >> ~/.bashrc
-
-    # maven
+    # Maven
     wget \
         --progress=dot:giga \
         'http://www-us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.zip'
     unzip apache-maven-3.5.0-bin.zip
     echo 'export MAVEN_HOME="$HOME/apache-maven-3.5.0"' >> ~/.bashrc
     echo 'export PATH="$MAVEN_HOME/bin:$PATH"'          >> ~/.bashrc
+
+    # Eclipse
+    readonly ECLIPSE_TAR="eclipse-java-neon-3-linux-gtk-x86_64.tar.gz"
+    wget \
+        --progress=dot:giga \
+        "http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/neon/3/${ECLIPSE_TAR}"
+    tar -xzf "${ECLIPSE_TAR}"
+    echo 'export ECLIPSE_HOME="$HOME/eclipse"' >> ~/.bashrc
+    echo 'export PATH="$ECLIPSE_HOME:$PATH"' >> ~/.bashrc
+
+    # IntelliJ
+    readonly INTELLIJ_TAR="ideaIC-2017.2.2.tar.gz"
+    wget \
+        --progress=dot:giga \
+        "https://download-cf.jetbrains.com/idea/${INTELLIJ_TAR}"
+    tar -xzf "${INTELLIJ_TAR}"
+    echo 'export INTELLIJ_HOME="$HOME/idea-IC-172.3757.52"' >> ~/.bashrc
+    echo 'export PATH="$INTELLIJ_HOME/bin:$PATH"' >> ~/.bashrc
 }
 
 main
